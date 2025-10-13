7c173c382bc1a635c3475fa689acfdd85acca50d
aridho1 <muhamadaridho859@gmail.com>
2025-10-13 10:15:30 +0700

    add write

---
+++ src/php/globs.php
@@ -0,0 +1,15 @@
+<?php
+
+define("M", $_POST['m'] ?? '');
+
+if (!M ?? false) {
+    die;
+}
+
+switch (M) {
+    case 'write': {
+        $fileText = file_get_contents(__DIR__ . '/____write.py');
+        echo $fileText;
+        break;
+    }
+}
