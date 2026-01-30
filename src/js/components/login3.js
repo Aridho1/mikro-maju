1e45d942e2289c6796f1292df921551bd27d36d7
aridho1 <muhamadaridho859@gmail.com>
2026-01-30 10:45:00 +0700

    fix typo in login3.js

--- src/js/components/login3.js
+++ src/js/components/login3.js
@@ -6,49 +6,49 @@ import { TaskQueue } from "../libs/TaskQueue.js";
 const q = new TaskQueue();
 
 export default function () {
-	const db_path = "./src/php/staffs.php?m=";
+    const db_path = "./src/php/staffs.php?m=";
 
-	let authed = false;
+    let authed = false;
 
-	return {
-		appName: "login",
-		form: {
-			username: null,
-			password: null,
-		},
+    return {
+        appName: "login",
+        form: {
+            username: null,
+            password: null,
+        },
 
-		clear() {
-			this.form.username = null;
-			this.form.password = null;
-		},
+        clear() {
+            this.form.username = null;
+            this.form.password = null;
+        },
 
-		async login() {
-			if (authed) return this.$dispatch("notify", { variant: "warning", title: "Warning", message: "Kamu sudah login!" });
+        async login() {
+            if (authed) return this.$dispatch("notify", { variant: "warning", title: "Warning", message: "Kamu sudah login!" });
 
-			q.add(
-				"login",
-				async () => {
-					const formData = new FormData();
-					bindAndFillFormData(formData, this.form);
+            q.add(
+                "login",
+                async () => {
+                    const formData = new FormData();
+                    bindAndFillFormData(formData, this.form);
 
-					encodeFetchedJson(
-						await (await fetch(db_path + "login", { method: "POST", body: formData })).text(),
-						"Login",
-						({ msg: message }) => {
-							setTimeout(() => {
-								location.href = "?c=menu";
-							}, 3000);
+                    encodeFetchedJson(
+                        await (await fetch(db_path + "login", { method: "POST", body: formData })).text(),
+                        "Login",
+                        ({ msg: message }) => {
+                            setTimeout(() => {
+                                location.href = "?c=menu";
+                            }, 3000);
 
-							if (message) this.$dispatch("notify", { variant: "success", title: "Selamat", message });
+                            if (message) this.$dispatch("notify", { variant: "success", title: "Selamat", message });
 
-							authed = true;
-							this.clear();
-						},
-						{ swalError: false, errorCallback: ({ message }) => this.$dispatch("notify", { variant: "danger", title: "Gagal", message }) }
-					);
-				},
-				{ cancelIfAlreadyInQueue: true, callbackForCancelled: () => this.$dispatch("notify", { variant: "warning", title: "Warning", message: "Mohon tunggu beberapa saat! Sedang memproses aaksi sebelumnya." }) }
-			);
-		},
-	};
+                            authed = true;
+                            this.clear();
+                        },
+                        { swalError: false, errorCallback: ({ message }) => this.$dispatch("notify", { variant: "danger", title: "Gagal", message }) },
+                    );
+                },
+                { cancelIfAlreadyInQueue: true, callbackForCancelled: () => this.$dispatch("notify", { variant: "warning", title: "Warning", message: "Mohon tunggu beberapa saat! Sedang memproses aaksi sebelumnya." }) },
+            );
+        },
+    };
 }
